0c06160c281274d1d765e41a22369bf500a0e572
tanish522 <tanishpatel0502@gmail.com>
2023-02-22 15:33:33 +0530

    approve_loan interface updated v1

--- src/Approve_Loan.java
+++ src/Approve_Loan.java
@@ -1,4 +1,28 @@
 public interface Approve_Loan {
-    abstract void savingAcLoan();
-    abstract void currentAcLoan();
+    // abstract method
+    void loanIsApproved(long balance);
+}
+
+class SavingAcLoan implements Approve_Loan{
+    public void loanIsApproved(long balance){
+        if(balance > 100000) {
+            long loanAmt = (long)(balance *1.5);
+            System.out.println("Loan is approved of " + loanAmt);
+        }
+        else{
+            System.out.println("Loan is not approved");
+        }
+    }
+}
+
+class CurrentAcLoan implements Approve_Loan{
+    public void loanIsApproved(long balance){
+        if(balance > 500000) {
+            long loanAmt = balance*3;
+            System.out.println("Loan is approved of " + loanAmt);
+        }
+        else{
+            System.out.println("Loan is not approved");
+        }
+    }
 }
