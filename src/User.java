cf9ff64d2bfdcb63be74f00f853fd4e2d6b83e18
tanish522 <tanishpatel0502@gmail.com>
2023-02-23 15:21:44 +0530

    deposit and withdraw method created v1

--- src/User.java
+++ src/User.java
@@ -1,57 +1,80 @@
 import java.util.Formatter;
 import java.util.HashMap;
+import java.util.Scanner;
 
-public class User
-{
+public class User {
     int Account_no;
-    String f_name,u_name,password;;
+    String f_name, u_name, password;
+    ;
     long balance;
     char acc_type;// c or s
-    User(){ }
-    User(int id,String name,String uname,String password,long bal, char type)
-    {
-        this.Account_no =2000;
-        this.f_name=name;
-        this.u_name=uname;
-        this.password=password;
-        this.balance=bal;
-        this.acc_type=type;
+
+    User() {
     }
 
-     boolean loginUser(HashMap<Integer,User> userDetails,String uname,String password)
-     {
-         if(userDetails.containsValue(uname))
-         {
-             return true;
-         }
-         else {
-             return false;
-         }
-     }
+    User(int id, String name, String uname, String password, long bal, char type) {
+        this.Account_no = 2000;
+        this.f_name = name;
+        this.u_name = uname;
+        this.password = password;
+        this.balance = bal;
+        this.acc_type = type;
+    }
+
+    boolean loginUser(HashMap<Integer, User> userDetails, String uname, String password) {
+        if (userDetails.containsValue(uname)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public User deposit(User u, Scanner sc) {
+        System.out.println("Enter amount you want to deposit: ");
+        int depositAmt = sc.nextInt();
+        u.balance += depositAmt;
+        return u;
+    }
+
+    public User withdraw(User u, Scanner sc) {
+        System.out.print("\nYour Balance: " + u.balance);
+        int withdrawAmt;
+        while (true) {
+            System.out.println("Enter amount you want to withdraw: ");
+            withdrawAmt = sc.nextInt();
+            if (withdrawAmt <= 0) {
+                System.out.println("Enter valid positive amount. ");
+            } else if (withdrawAmt > u.balance) {
+                System.out.print("\nInsufficient balance. Enter valid amount.");
+            } else {
+                break;
+            }
+        }
+        u.balance -= withdrawAmt;
+        return u;
+    }
 }
-class UserData
-{
-    HashMap<Integer,User> userDetails =new HashMap<>();
 
-    public UserData()
-    {
-        userDetails.put(1,new User(1,"Neel Shah","neel","neel",2000000L,'c'));
-        userDetails.put(2,new User(2,"Pratish Soni","pratish","pratish",100000L,'s'));
-        userDetails.put(3,new User(3,"Tanish Patel","tanish","tanish",250000L,'c'));
-        userDetails.put(4,new User(4,"Jinal Thakor","jinal","jinal",200000L,'s'));
+class UserData {
+    HashMap<Integer, User> userDetails = new HashMap<>();
+
+    public UserData() {
+        userDetails.put(1, new User(1, "Neel Shah", "neel", "neel", 2000000L, 'c'));
+        userDetails.put(2, new User(2, "Pratish Soni", "pratish", "pratish", 100000L, 's'));
+        userDetails.put(3, new User(3, "Tanish Patel", "tanish", "tanish", 250000L, 'c'));
+        userDetails.put(4, new User(4, "Jinal Thakor", "jinal", "jinal", 200000L, 's'));
     }
-    public void getData(User user)
-    {
-        Formatter fmt=new Formatter();
-        fmt.format("\n%10 %15 %15","Account Number","Full Name","User Name");
+
+    public void getData(User user) {
+        Formatter fmt = new Formatter();
+        fmt.format("\n%10 %15 %15", "Account Number", "Full Name", "User Name");
         System.out.println("\n------------------------------------------------------------\n");
-        fmt.format("%10 %15 %15",user.Account_no,user.f_name,user.u_name);
+        fmt.format("%10 %15 %15", user.Account_no, user.f_name, user.u_name);
         System.out.println(fmt);
 
     }
 
-    public User getUser(String name)
-    {
+    public User getUser(String name) {
 
     }
 }
